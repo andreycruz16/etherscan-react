4d92f450f0949c9c649602f881f0695d8fc83c19
Mark Andrey <markandreydelacruz@ymail.com>
2017-12-07 12:46:03 +0800

    update

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -13,22 +13,22 @@ import {
 } from 'reactstrap';
 
 class Dashboard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-    }
-    this.fetchAPI();
-  } 
+	constructor() {
+		super();
+		this.state = {
+			data: [],
+		}
+		this.fetchAPI();
+	}
 
   fetchAPI() {
-  fetch('https://api.nanopool.org/v1/eth/payments/0x2bedd2529f550c2cc2146633c738cbb7945adaef').then((Response) => Response.json()).then((findresponse) => {
-      console.log(findresponse);
-      this.setState({
-        data: [findresponse.data]
-      });
-    })
-  } 
+  	fetch('https://api.nanopool.org/v1/eth/payments/0x2bedd2529f550c2cc2146633c738cbb7945adaef').then((Response) => Response.json()).then((findresponse) => {
+  		console.log(findresponse);
+  		this.setState({
+  			data: [findresponse.data],
+  		});
+  	})
+  }
 
   render() {
     return (
@@ -37,7 +37,7 @@ class Dashboard extends Component {
           <Col>
             <Card>
               <CardHeader>
-                <i className="fa fa-align-justify"></i> Etherscan.io (api.nanopool.org/v1/eth/payments/0x2bedd2529f550c2cc2146633c738cbb7945adaef)
+                <i className="fa fa-align-justify"></i> Etherscan.io (<a href="https://api.nanopool.org/v1/eth/payments/0x2bedd2529f550c2cc2146633c738cbb7945adaef" title="API" target="_blank">https://api.nanopool.org/v1/eth/payments/0x2bedd2529f550c2cc2146633c738cbb7945adaef</a>)
               </CardHeader>
               <CardBody>
                 <Table hover bordered striped responsive size="sm">
@@ -56,17 +56,17 @@ class Dashboard extends Component {
 			            return keys.map(data => {
 			              let date = new Date(dynamicData[data].date * 1000);
 			              let hashLink = "https://etherscan.io/tx/" + dynamicData[data].txHash;
-			              return (      
-			              	<tr key={dynamicData[data].txHash}>          
-			                  <td>{date.toString().substring(4,15)}</td>
-			                  <td><a href={ hashLink } target="_blank">{dynamicData[data].txHash}</a></td>
-			                  <td>{dynamicData[data].amount}</td>
-			                  <td>{String(dynamicData[data].confirmed).toUpperCase()}</td>
+			              return (
+			              	<tr key={ dynamicData[data].txHash }>
+			                  <td>{ String(date).substring(4,15) }</td>
+			                  <td><a href={ hashLink } target="_blank">{ dynamicData[data].txHash }</a></td>
+			                  <td>{ dynamicData[data].amount }</td>
+			                  <td class="text-center">{ String(dynamicData[data].confirmed).toUpperCase() }</td>
 			                </tr>
 			              );
 			            });
 			          })
-			        }   
+			        }
                   </tbody>
                 </Table>
               </CardBody>
